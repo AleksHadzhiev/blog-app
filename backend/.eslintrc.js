32f677edc581e70db5733aa4dbac8742230fbf5b
AleksHadzhiev <aleksandar@d-centralize.nl>
2024-03-02 11:17:33 +0100

    fix(script): change it so ti does not check node_modules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', './node_modules/@typescript-eslint/scope-manager/dist/referencer/ClassVisitor.js'],
+  ignorePatterns: ['.eslintrc.js', './node_modules'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
